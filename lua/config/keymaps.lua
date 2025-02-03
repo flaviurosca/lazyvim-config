e419f1316966f32f297158eae6bbe27a24ea640a
flaviurosca <flaviuroscaivanciuc@gmail.com>
2025-02-03 15:32:42 +0200

    delete unused keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- vim.keymap.set(
---   "n",
---   "<leader>sx",
---   require("telescope.builtin").resume,
---   { noremap = true, silent = true, desc = "Resume" }
--- )
-
 -- Exit modes using 'kj'
 -- vim.keymap.set({ "i", "v", "c", "t", "o" }, "kj", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set({ "i", "v", "c", "t", "o" }, "kj", function()
